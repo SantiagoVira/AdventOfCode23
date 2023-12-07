a7a3614be58ff87479c4bd1d7cf3f493d93d8f43
SantiagoVira <santiagovira06@gmail.com>
2023-12-07 01:19:34 -0500

    literally why

--- Day7/part2.py
+++ Day7/part2.py
@@ -3,7 +3,7 @@ import collections
 with open("./Day7/input.txt") as f:
     data = [line.strip() for line in f.readlines()]
 
-order = "AKQJT98765432"
+order = "AKQT98765432J"
 
 typed_hands = {
     "five": [],
@@ -19,22 +19,28 @@ for line in data:
     [hand, bid] = line.split(" ")
     bid = int(bid)
 
-    if len(set(hand)) == 1:
+    most_common_letter = collections.Counter(hand).most_common()[0][0]
+    new_hand = hand.replace("J", most_common_letter)
+
+    if hand != new_hand:
+        print(hand, new_hand)
+
+    if len(set(new_hand)) == 1:
         typed_hands["five"].append((hand, bid))
-    elif len(set(hand)) == 2:
-        if hand.count(hand[0]) in [1, 4]:
+    elif len(set(new_hand)) == 2:
+        if new_hand.count(new_hand[0]) in [1, 4]:
             typed_hands["four"].append((hand, bid))
         else:
             typed_hands["full"].append((hand, bid))
-    elif len(set(hand)) == 3:
-        num_of_most_common = collections.Counter(hand).most_common()[0][1]
+    elif len(set(new_hand)) == 3:
+        num_of_most_common = collections.Counter(new_hand).most_common()[0][1]
         if num_of_most_common == 3:
             typed_hands["three"].append((hand, bid))
         else:
             typed_hands["twopair"].append((hand, bid))
-    elif len(set(hand)) == 4:
+    elif len(set(new_hand)) == 4:
         typed_hands["onepair"].append((hand, bid))
-    elif len(set(hand)) == 5:
+    elif len(set(new_hand)) == 5:
         typed_hands["high"].append((hand, bid))
 
 res = 0
@@ -45,8 +51,11 @@ rank = 1000
 for hand_type, hands in typed_hands.items():
     sorted_hands = sorted(hands, key=lambda hand: [
                           order.index(c) for c in hand[0]])
+    print(sorted_hands)
     for hand in sorted_hands:
         res += rank * hand[1]
         rank -= 1
 
 print(res)
+
+# Wrong: 248368409
