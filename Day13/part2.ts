b7d1d3a9d844e5d0710b42414852c34fde28e035
SantiagoVira <santiagovira06@gmail.com>
2023-12-13 13:17:07 -0500

    finish day 13

--- Day13/part2.ts
+++ Day13/part2.ts
@@ -3,19 +3,24 @@ import fs from "fs";
 const patterns = fs.readFileSync("./input.txt", "utf-8").split("\n\n");
 
 let sum = 0;
-// 35135 too high
-// 33975
+// 36822 too high, but sample code works
+// 8445 too low whe doing columns first, which doesnt work for sample code
+
 const areEq = (list1: string[], list2: string[]) => {
   const minLen = Math.min(list1.length, list2.length);
-  return (
-    JSON.stringify(list1.slice(0, minLen)) ===
-    JSON.stringify(list2.slice(0, minLen))
-  );
+  const string1 = JSON.stringify(list1.slice(0, minLen));
+  const string2 = JSON.stringify(list2.slice(0, minLen));
+  const diffs = string1.split("").filter((char, i) => char !== string2[i]);
+  return diffs.length === 1;
 };
 
 for (let pattern of patterns) {
   const rows = pattern.split("\n");
   let didAdd = false;
+  const cols = rows[0]
+    .split("")
+    .map((_, x) => rows.map((row, i) => row[x]).join(""));
+
   for (let y = 1; y < rows.length; y++) {
     if (areEq(rows.slice(0, y).reverse(), rows.slice(y))) {
       sum += y * 100;
@@ -25,9 +30,6 @@ for (let pattern of patterns) {
   }
   if (didAdd) continue;
 
-  const cols = rows[0]
-    .split("")
-    .map((_, x) => rows.map((row, i) => row[x]).join(""));
   for (let x = 1; x < cols.length; x++) {
     if (areEq(cols.slice(0, x).reverse(), cols.slice(x))) {
       sum += x;
