2331f36753ae042dee0da585467bed53eeab4a31
SantiagoVira <santiagovira06@gmail.com>
2023-12-15 00:23:38 -0500

    day 15 part 1

---
+++ Day15/part1.ts
@@ -0,0 +1,15 @@
+import fs from "fs";
+
+const data = fs.readFileSync("./input.txt", "utf-8").split(",");
+
+let sum = 0;
+for (let j = 0; j < data.length; j++) {
+  let current = 0;
+  for (let i = 0; i < data[j].length; i++) {
+    current += data[j][i].charCodeAt(0);
+    current = (current * 17) % 256;
+  }
+  sum += current;
+}
+
+console.log(sum);
